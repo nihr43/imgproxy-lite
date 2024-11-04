bf810d3264e990594a636c1e6f0077e02c30eb25
nathan hensel <nathanihensel@gmail.com>
2024-11-04 16:35:46 -0600

    implement naive caching

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_file
 from PIL import Image
 import requests
 import time
+import uuid
 from io import BytesIO
 
 app = Flask(__name__)
@@ -13,11 +14,25 @@ app = Flask(__name__)
 @app.route("/")
 def convert():
     image_url = f"https://cdn.nih.earth/web-assets/{request.args.get('img')}"
-    try:
-        response = requests.get(image_url)
-        response.raise_for_status()
+    cache_key = str(uuid.uuid5(uuid.NAMESPACE_OID, request.url))
 
-        image_stream = BytesIO(response.content)
+    try:
+        with open(cache_key, "rb") as f:
+            image_stream = BytesIO(f.read())
+
+        print("cache hit")
+        image_stream.seek(0)
+        return send_file(image_stream, mimetype="image/jpeg")
+    except FileNotFoundError:
+        print("cache miss")
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()
+            image_stream = BytesIO(response.content)
+
+        except requests.exceptions.RequestException as e:
+            return f"Failed to fetch image: {e}", 500
+
         image_obj = Image.open(image_stream)
         image_stream.seek(0)
 
@@ -30,9 +45,10 @@ def convert():
             elapsed = time.time() - start
             print(f"converting took {elapsed}")
 
+            with open(cache_key, "wb") as f:
+                f.write(membuf.getvalue())
+
+            membuf.seek(0)
             return send_file(membuf, mimetype="image/jpeg")
         except TypeError:
             return send_file(image_stream, mimetype="image/jpeg")
-
-    except requests.exceptions.RequestException as e:
-        return f"Failed to fetch image: {e}", 500
