10864863755f1e3c2dd7b01482acda471707e47d
nathan hensel <nathanihensel@gmail.com>
2024-11-16 15:30:02 -0600

    Revert "attempt at leader election"
    This reverts commit d86ef0a7591b37d7f6990ef51e195508e863a845.

--- app.py
+++ app.py
@@ -5,51 +5,8 @@ import time
 import uuid
 import os
 from io import BytesIO
-from kubernetes import client, config
-from datetime import datetime
 
 app = Flask(__name__)
-
-try:
-    config.load_incluster_config()
-except config.config_exception.ConfigException:
-    print("not in cluster")
-
-_namespace = "default"
-_lease_name = "imgproxy-lite"
-_identity = os.getenv("HOSTNAME")
-
-
-def lock():
-    v1 = client.CoordinationV1Api()
-    try:
-        lease = v1.read_namespaced_lease(_lease_name, _namespace)
-        if (
-            lease.spec.holder_identity is None
-            or lease.spec.holder_identity == _identity
-        ):
-            lease.spec.holder_identity = _identity
-            lease.spec.renew_time = datetime.utcnow().isoformat() + "Z"
-            v1.replace_namespaced_lease(_lease_name, _namespace, lease)
-            print(f"lock aquired: {_identity}")
-            return True
-    except client.exceptions.ApiException as e:
-        if e.status == 404:
-            lease = client.V1Lease(
-                metadata=client.V1ObjectMeta(name=_lease_name),
-                spec=client.V1LeaseSpec(
-                    holder_identity=_identity,
-                    acquire_time=datetime.utcnow().isoformat() + "Z",
-                    renew_time=datetime.utcnow().isoformat() + "Z",
-                    lease_duration_seconds=10,
-                ),
-            )
-            v1.create_namespaced_lease(_namespace, lease)
-            print(f"lease created: {_identity}")
-            return True
-    except Exception as e:
-        print(f"error seeking lock: {e}")
-    return False
 
 
 def prune_cache(directory, limit=100):
@@ -84,8 +41,7 @@ def convert():
         return send_file(f"artifacts/{cache_key}", mimetype="image/jpeg")
     except (FileNotFoundError, IOError, OSError, EOFError):
         print("cache miss")
-        if lock():
-            prune_cache("artifacts")
+        prune_cache("artifacts")
         try:
             response = requests.get(image_url)
             response.raise_for_status()
--- default.nix
+++ default.nix
@@ -5,7 +5,6 @@ let
     flask
     requests
     pillow
-    kubernetes
   ]);
 in
 
