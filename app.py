0d24212e0cb7058eda43afb4b9434395560d6992
nathan hensel <nathanihensel@gmail.com>
2024-11-17 19:22:52 -0600

    accept default threads

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
     args = parser.parse_args()
 
     if args.serve:
-        waitress.serve(app, host="0.0.0.0", port=5000, threads=2)
+        waitress.serve(app, host="0.0.0.0", port=5000)
     elif args.gc:
         gc()
     else:
